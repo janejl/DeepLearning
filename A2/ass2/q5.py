17f5e62f36090f8741936e88fecee92ac41a99d7
janejl <jane.hkucs@gmail.com>
2020-05-01 01:10:26 +0800

    add model definition in q5.py

--- A2/ass2/q5.py
+++ A2/ass2/q5.py
@@ -1,10 +1,66 @@
 from keras import layers
 from keras import models
+from keras import optimizers
 from keras.applications import VGG16
 import json
 import os
 import shutil
 from keras.preprocessing.image import ImageDataGenerator
+
+
+def best_model():
+    conv_base = VGG16(weights='imagenet', include_top=False, input_shape=(224, 224, 3))
+    set_trainable = False
+    for layer in conv_base.layers:
+        if layer.name == 'block5_conv1':
+            set_trainable = True
+        layer.trainable = set_trainable
+    
+    model = models.Sequential()
+    model.add(conv_base)
+    model.add(layers.Flatten())
+    model.add(layers.Dense(512, activation='relu'))
+    model.add(layers.Dropout(0.5))
+    model.add(layers.BatchNormalization())
+    model.add(layers.Dense(4, activation='softmax'))
+
+    model.compile(loss='categorical_crossentropy',
+              optimizer=optimizers.RMSprop(learning_rate=1e-4),
+              metrics=['acc'])
+    return model
+
+def train_and_save_model():
+    aug_train_datagen = ImageDataGenerator(
+        rescale=1./255,
+        rotation_range=40,
+        width_shift_range=0.2,
+        height_shift_range=0.2,
+        shear_range=0.2,
+        zoom_range=0.2,
+        horizontal_flip=True,
+        fill_mode='nearest')
+    aug_train_generator = aug_train_datagen.flow_from_directory(
+        train_dir,
+        target_size=(224, 224),
+        batch_size=32,
+        class_mode='categorical')
+    val_datagen = ImageDataGenerator(rescale=1./255)
+    val_generator = val_datagen.flow_from_directory(
+        val_dir,
+        target_size=(224, 224),
+        batch_size=32,
+        class_mode='categorical')
+
+    model = best_model()
+    history = model.fit_generator(
+        aug_train_generator,
+        steps_per_epoch=100,
+        epochs=30,
+        validation_data=val_generator,
+        validation_steps=50)
+
+    model.save('/userhome/34/ljiang/deep_learning/A2/ass2/q5_model/q5_model')
+
 
 def predict(test_dir, output_dir="q5_result"):
     """
@@ -25,7 +81,7 @@ def predict(test_dir, output_dir="q5_result"):
         raise Exception("Model not found.")
 
     ### Restore your model
-    model = models.load_model('/userhome/34/ljiang/deep_learning/A2/ass2/q5_model')
+    model = models.load_model('/userhome/34/ljiang/deep_learning/A2/ass2/q5_model/q5_model')
 
     ### Make predictions with restored model
     test_datagen = ImageDataGenerator(rescale=1./255)
