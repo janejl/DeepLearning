3fd1af63bd220c657b5a5b678c428a9bf1b07106
janejl <jane.hkucs@gmail.com>
2020-04-30 17:27:12 +0800

    narrow down range

--- A2/ass2/q3.py
+++ A2/ass2/q3.py
@@ -4,7 +4,7 @@ def learning_rate_range():
     proper learning rate"""
     # Lower and upper bounds
     #######
-    lower_bound, upper_bound = 0.1, 1e-9
+    lower_bound, upper_bound = 0.1, 1e-6
     #######
     return lower_bound, upper_bound
 
