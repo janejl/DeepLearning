18bbad89ac56ba04f8bb8ce1429338132709932b
Jiang Ling <ljiang@gpugate1.gpufarm.cs.hku.hk>
2020-05-01 00:29:17 +0800

    add padding to Q2

--- A2/ass2/q2.py
+++ A2/ass2/q2.py
@@ -24,13 +24,13 @@ def modified_cnn():
     #######
     model = models.Sequential()
     
-    model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(150, 150, 3)))
+    model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(150, 150, 3), padding='same'))
     model.add(layers.MaxPooling2D((2, 2)))
-    model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+    model.add(layers.Conv2D(64, (3, 3), activation='relu', padding='same'))
     model.add(layers.MaxPooling2D((2, 2)))
-    model.add(layers.Conv2D(128, (3, 3), activation='relu'))
+    model.add(layers.Conv2D(128, (3, 3), activation='relu', padding='same'))
     model.add(layers.MaxPooling2D((2, 2)))
-    model.add(layers.Conv2D(128, (3, 3), activation='relu'))
+    model.add(layers.Conv2D(128, (3, 3), activation='relu', padding='same'))
     model.add(layers.MaxPooling2D((2, 2)))
 
     model.add(layers.Flatten())
